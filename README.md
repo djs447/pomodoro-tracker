ee88248d4bcb7156ff304f110d6c8da259786deb
Daniel <djs0529@gmail.com>
2025-03-22 02:41:46 -0400

    Added dark mode, fixed graphics and timing bugs

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This app generates a timer that by default assigns a 25-minute window to working
 
 This app follows the principles of the Pomodoro technique, where the practitioner will practice working and resting in 25 and 5 minute windows to improve concentration and mitigate burnout. 
 
+For this build, I added a toggle for dark-mode and light-mode. This toggle switches the theme for the application.
+
 ## Alerts
 
 If the user attempts to increase or decrease either time window outside of the [1,60] range, a blue alert window appears notifying them of the valid range.
@@ -12,6 +14,10 @@ If the user attempts to change the timer's settings while the timer is running, 
 
 ## Known Issues
 
-While tabbed out, it appears that the timer slows / stops. This is due to the browser de-prioritizing the app while it is out of focus. In the next build this will be fixed with the Date() object.
+~~While tabbed out, it appears that the timer slows / stops. This is due to the browser de-prioritizing the app while it is out of focus. In the next build this will be fixed with the Date() object.~~
 
-Currently the completion percentage of the progress bar is inaccurate at the start of the timer and becomes more accurate as the timer fills out. This is believed to be an issue with the dasharray offset, and alternatives are being explored to create a more accurate progress bar.
+- This was caused by the setInterval() being de-prioritized while the tab was closed. Switching to using the Date() object helped this, however it also caused issues that prevented the timer from alternating between Work and Rest settings while tabbed out. At first this was the timer overflowing and going back to 60m, and then it not progressing on the rest timer at all. This was fixed by tracking the negative amount logged in remainingTime and then adding that to the new Date object created when transitioning to the alternate timer.
+
+~~Currently the completion percentage of the progress bar is inaccurate at the start of the timer and becomes more accurate as the timer fills out. This is believed to be an issue with the dasharray offset, and alternatives are being explored to create a more accurate progress bar.~~
+
+- Swapping to the progress bar provided by MUI instead of the custom one created by myself the percentage bar was fixed.
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 import * as d3 from "d3";
 import Badge from 'react-bootstrap/Badge'
 import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faArrowUp, faPlay, faPause, faArrowsRotate} from '@fortawesome/free-solid-svg-icons'
+import CircularProgress from '@mui/material/CircularProgress'
+import Switch from '@mui/material/Switch'
 import {faArrowDown} from '@fortawesome/free-solid-svg-icons'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
@@ -16,51 +19,114 @@ function App() {
   const [showActiveAlert, setShowActiveAlert] = useState(false);
   const [countdownActive, setCountdownActive] = useState(false);
   const [workExpired, setWorkExpired] = useState(false);
-  const [timeLeft, setTimeLeft] = useState([workTimer,0]);
+  const [timeLeft, setTimeLeft] = useState(new Date(0,0,0,0,workTimer,0));
+  const [endTime, setEndTime] = useState(null);
   const [dashOffset, setDashOffset] = useState(0);
   const [restOffset, setRestOffset] = useState(0);
-
-  useEffect(()=> {
-    if(!countdownActive && !workExpired){
-      setTimeLeft([workTimer,timeLeft[1]]);
-    }
-    if(!countdownActive && workExpired){
-      setTimeLeft([restTimer, timeLeft[1]]);
-    }
-  },[workTimer,restTimer]);
+  const [isDark, setIsDark] = useState(true);
+  const frameRef = useRef(null);
 
   useEffect(()=>{
     setDashOffset(0);
     setRestOffset(0);
   },[workExpired])
 
+  useEffect(()=>{
+    setTimeLeft(new Date(0,0,0,0,workTimer,0));
+  },[workTimer]);
+
+  useEffect(()=>{
+    if(isDark){
+      document.body.classList.add('dark');
+    } else{
+      document.body.classList.remove('dark');
+    }
+  }, [isDark]);
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.hidden || !countdownActive) return;
+  
+      const now = new Date();
+      setTimeLeft(new Date(endTime - now));
+    };
+  
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+  
+    return () => document.removeEventListener("visibilitychange", handleVisibilityChange);
+  }, [countdownActive, endTime]);
+
   useEffect(() => {
 
-    let interval;
+    const updateTimer = () =>{
+      if (!countdownActive || !endTime){
+        return;
+      }
+
+      const now = new Date();
+      const remainingTime = endTime - now;
+
+      if (remainingTime <= 0){
+        if(!workExpired){
+          setWorkExpired(true);
+          setEndTime(new Date(now.getTime() + restTimer*60000 + remainingTime));
+        }
+        else{
+          setWorkExpired(false);
+          setEndTime(new Date(now.getTime() + workTimer * 60000 + remainingTime));
+        }
+      }
+      else{
+        setTimeLeft(new Date(remainingTime));
+        frameRef.current = requestAnimationFrame(updateTimer);
+      }
+
+      if(workExpired){
+        setRestOffset(calculateRestOffset(timeLeft));
+      }
+      else{
+        setDashOffset(calculateDashOffset(timeLeft));
+      }
+
+    };
 
     if(countdownActive){
-      interval = setInterval(() => {
-        setTimeLeft((prevTimeLeft) =>{
-          const newTimeLeft = calculateTimeLeft(prevTimeLeft);
-          if(!workExpired){
-            setDashOffset(calculateDashOffset(newTimeLeft));
-          }
-          else{
-            setRestOffset(calculateRestOffset(newTimeLeft));
-          }
-          return newTimeLeft;
-        });
-      }, 1000);
+      frameRef.current = requestAnimationFrame(updateTimer);
+    }
+    else{
+      cancelAnimationFrame(frameRef.current);
+      frameRef.current = null;
     }
 
+    return () => cancelAnimationFrame(frameRef.current);
 
+  },[countdownActive, endTime, workExpired, timeLeft]);
 
-    return () => clearInterval(interval);
+  function toggleCountdown(){
 
-  },[countdownActive, timeLeft]);
+    if(countdownActive){
+      cancelAnimationFrame(frameRef.current);
+      frameRef.current = null;
+      setCountdownActive(false);
+
+      const now = new Date();
+      const elapsedTime = now - new Date(endTime);
+      setEndTime(new Date(endTime - elapsedTime));
+    }
+    else{
+      setTimeout(()=> {
+          const now = new Date();
+          const newEndTime = new Date(now.getTime() + timeLeft.getMinutes() * 60000 + timeLeft.getSeconds()*1000);
+
+          setEndTime(newEndTime);
+          setCountdownActive(true);
+      }, 200);
+    }
+  }
 
   function incrementWorkTimer(){
     if(workTimer<60 && !countdownActive){
+      resetClock();
       return setWorkTimer(workTimer+1);
     }
     else if(countdownActive){
@@ -76,6 +142,7 @@ function App() {
 
   function decrementWorkTimer(){
     if(workTimer>1 && !countdownActive){
+      resetClock();
       return setWorkTimer(workTimer-1);
     }
     else if(countdownActive){
@@ -91,6 +158,7 @@ function App() {
 
   function incrementRestTimer(){
     if(restTimer<60 && !countdownActive){
+      resetClock();
       return setRestTimer(restTimer+1);
     }
     else if(countdownActive){
@@ -105,6 +173,7 @@ function App() {
 
   function decrementRestTimer(){
     if(restTimer>1 && !countdownActive){
+      resetClock();
       return setRestTimer(restTimer-1);
     }
     else if(countdownActive){
@@ -131,86 +200,51 @@ function App() {
     },3000);
   }
 
-  function toggleCountdown(){
-    return setCountdownActive(!countdownActive);
-  }
-
-  function calculateTimeLeft(p){
-    let minutes = p[0];
-    let seconds = p[1];
-
-    if(minutes === 0 && seconds === 0 && !workExpired){
-      setWorkExpired(true);
-      return [restTimer,0];
-    }
-    else if(minutes === 0 && seconds === 0 && workExpired){
-      setWorkExpired(false);
-      return [workTimer,0];
-    }
-
-    if(seconds === 0){
-      return [minutes-1,59];
-    }
-
-    return [minutes,seconds-1];
-
-
-  }
-
   function calculateDashOffset(p){
-    const offset = Math.round((workTimer*60 - (p[0]*60 + p[1]))/(workTimer*60)*900)
-    if(offset < 0){
-      return 0;
-    }
-    else{
-      return offset;
-    }
+    const minutes = p.getMinutes();
+    const seconds = p.getSeconds();
+    const offset = (((minutes*60+seconds) * 100) / (workTimer*60));
+    return offset;
   }
 
   function calculateRestOffset(p){
-    const offset = Math.round((restTimer*60 - (p[0]*60 + p[1]))/(restTimer*60)*900);
-    if(offset < 0){
-      return 0;
-    }
-    else{
-      return offset;
-    }
+    const minutes = p.getMinutes();
+    const seconds = p.getSeconds();
+    const offset = (((minutes*60+seconds) * 100) / (restTimer*60));
+    return offset;
   }
 
   function resetClock(){
+    cancelAnimationFrame(frameRef.current);
+    frameRef.current = null;
     setCountdownActive(false);
-    setTimeLeft([workTimer,0]);
+    setTimeLeft(new Date(0,0,0,0,workTimer,0));
     setWorkExpired(false);
     setDashOffset(0);
+    setEndTime(null);
   }
 
-
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: "(prefers-color-scheme: dark)",
+    },
+    undefined,
+    (isSystemDark) => setIsDark(isSystemDark)
+  );
 
   return (
     <>
+      <div className="day-night-toggle">
+        <h4><Badge bg="secondary">Dark Mode Toggle</Badge></h4>
+        <Switch defaultChecked onChange={({ target }) => setIsDark(target.checked)} />
+      </div>
       <div className="container">
         <h2 className="title-text"><Badge bg={workExpired ? "success" : "primary"}>Pomodoro Clock</Badge></h2>
+        <h2 className="title-text"><Badge bg={workExpired ? "success" : "primary"}>{workExpired ? "WORK" : "REST"}</Badge></h2>
         <div className="timer-wrapper">
           <div className="timer">
-            <div className="outer-bar">
-              <div className="inner-bar">
-                {timeLeft[0]}m {timeLeft[1]}s
-              </div>
-              <svg className="progress-bar" xmlns="http://www.w3.org/2000/svg" version="1.1" width="320px" height="320px">
-                  <defs>
-                      <linearGradient id="GradientColor">
-                          <stop offset="0%" stopColor="#599cff" />
-                          <stop offset="100%" stopColor="#003480" />
-                      </linearGradient>
-                      <linearGradient id="restColor">
-                          <stop offset="0%" stopColor="#21b300" />
-                          <stop offset="100%" stopColor="#136600" />
-                      </linearGradient>
-                  </defs>
-                  {workExpired ? (<circle cx="160" cy="160" r="140" stroke="url(#restColor)" strokeDasharray= "900" strokeDashoffset={restOffset} style={{ transition: "stroke-dashoffset 0.1s linear"}} />) :
-                  (<circle cx="160" cy="160" r="140" stroke="url(#GradientColor)" strokeDasharray= "900" strokeDashoffset={dashOffset} style={{ transition: "stroke-dashoffset 0.1s linear", transform:"rotate(-90deg)", transformOrigin: "50% 50%"}} /> ) };
-              </svg>
-            </div>
+              <CircularProgress className="prog-bar" color={workExpired ? "success" : "primary"} variant="determinate" size="400px" value={(workExpired ? Math.round(restOffset) : Math.round(dashOffset)) || 100} />
+              <span className={`middle-text${isDark ? " dark" : ""}`}>{timeLeft.getMinutes()}m {timeLeft.getSeconds()}s</span>
           </div>
           <div className="timer-controls">
             <Button className="controls" onClick={toggleCountdown} variant={workExpired ? "success" : "primary"}><FontAwesomeIcon icon={countdownActive ? faPause : faPlay} /></Button>
@@ -220,13 +254,13 @@ function App() {
             <div className="minutes">
               <h4><Badge bg={workExpired ? "success" : "primary"}>Working Minutes</Badge></h4>
               <button onClick={incrementWorkTimer}><FontAwesomeIcon icon={faArrowUp}/></button> 
-              <span className="setTime">{workTimer}</span>
+              <span className={`setTime${isDark ? " dark" : ""}`}>{workTimer}</span>
               <button onClick={decrementWorkTimer}><FontAwesomeIcon icon={faArrowDown}/></button>
             </div>
             <div className="seconds">
               <h4><Badge bg={workExpired ? "success" : "primary"}>Rest Minutes</Badge></h4>
               <button onClick={incrementRestTimer}><FontAwesomeIcon icon={faArrowUp}/></button> 
-              <span className="setTime">{restTimer}</span>
+              <span className={`setTime${isDark ? " dark" : ""}`}>{restTimer}</span>
               <button onClick={decrementRestTimer}><FontAwesomeIcon icon={faArrowDown}/></button>
             </div>
           </div>
